d0f4af64bed18c56532e60153d8e73984c58ae89
sugarsword <63600340+sugarsword@users.noreply.github.com>
2020-04-16 09:54:43 +0200

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -15,9 +15,9 @@ device=""
 SECONDS=0
 
 (cd $tools/pytorch-examples/word_language_model &&
-    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py --data $data/trump \
+    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py --data $data/dylan \
         --epochs 40 \
-        --emsize 200 --nhid 200 --dropout 0.5 --tied \
+        --emsize 100 --nhid 100 --dropout 0.5 --tied \
         --save $models/model.pt
 )
 
